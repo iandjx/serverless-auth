60a66bc14aa9a942f87d6e51a8ee8c1554ba9b06
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-06-03 06:58:57 +0900

    intial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  async function fetchProfile() {
+    await fetch("/.netlify/functions/auth/me").then((res) =>
+      res.json().then((res) => setCurrentUser(res))
+    );
+  }
+
+  async function fetchLogout() {
+    await fetch("/.netlify/functions/auth/logout").then(setCurrentUser({}));
+  }
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {currentUser && currentUser && currentUser.id ? (
+          <>
+            <img src={currentUser.id} className="App-logo" alt="logo" />
+            <p>Hello {currentUser.name}</p>
+            <div className="App-link" onClick={fetchLogout}>
+              Logout
+            </div>
+          </>
+        ) : (
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Sample demo to show Netlify login with PassportJS</p>
+            <a
+              className="App-link"
+              href={`/.netlify/functions/auth/github?host=${
+                window.location.origin
+              }`}
+            >
+              Login with Github
+            </a>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lambda/lib/strategies/github.js
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import passport from "passport";
+import { Strategy } from "passport-github";
+import get from "lodash/fp/get";
+
+const router = Router();
+
+router.use((req, _res, next) => {
+  const {
+    query: { host },
+  } = req;
+
+  // Note: Netlify functions don't have the host url, so we need to pass it explicitly
+  if (!passport._strategy(Strategy.name) && host) {
+    console.info(`Init Github Auth strategy on host ${host}`);
+
+    passport.use(
+      new Strategy(
+        {
+          clientID: process.env.GITHUB_AUTH_CLIENT_ID,
+          clientSecret: process.env.GITHUB_AUTH_CLIENT_SECRET,
+          callbackURL: `${host}/.netlify/functions/auth/github/callback`,
+          passReqToCallback: true,
+        },
+        async function(req, _token, _tokenSecret, profile, done) {
+          console.info("load user profile", profile);
+          const user = {
+            id: profile.id,
+            name: profile._json.name,
+          };
+
+          req.user = user;
+          return done(null, user);
+        }
+      )
+    );
+  }
+  next();
+});
+
+router.get(
+  "/github",
+  passport.authenticate("github", {
+    scope: ["profile", "repo"],
+  })
+);
+
+router.get(
+  "/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  function callback(req, res) {
+    console.info(`login user ${req.user && req.user.id} and redirect`);
+
+    return req.login(req.user, async function callbackLogin(loginErr) {
+      if (loginErr) {
+        throw loginErr;
+      }
+      return res.redirect("/");
+    });
+  }
+);
+
+export default router;
