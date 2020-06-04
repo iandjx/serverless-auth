a30405b8b5d02dd32c0bff5b59557fb983fbb6e3
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-06-04 14:36:12 +0900

    added return statement

--- src/lambda/lib/strategies/github.js
+++ src/lambda/lib/strategies/github.js
@@ -146,7 +146,7 @@ router.use((req, _res, next) => {
             })
             .catch((error) => console.log(error));
           console.log(req.user);
-          done(null, user);
+          return done(null, user);
         }
       )
     );
